49b5df635d838dc9b82ebb1649de73a0965900e4
IEQDaniel <Daniel@ieqtechnology.com>
2018-08-13 18:02:37 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyAoBSFYczOaBjAfeHRQeXG6JrOzs8Uu7As';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
 
 //Create new component. This component should produce some HTML
 
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //Take this components generated html and put in in the DOm
